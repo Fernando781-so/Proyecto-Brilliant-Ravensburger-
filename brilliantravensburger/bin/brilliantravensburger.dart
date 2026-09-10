87b473f8f91fbf5238ee472f2a101a40111c2c5c
Fernando781-so <feryhfjkg@gmail.com>
2026-09-09 17:56:27 -0700

    Inicio de la franquicia

---
+++ brilliantravensburger/bin/brilliantravensburger.dart
@@ -0,0 +1,5 @@
+import 'package:brilliantravensburger/brilliantravensburger.dart' as brilliantravensburger;
+
+void main(List<String> arguments) {
+  print('Hello world: ${brilliantravensburger.calculate()}!');
+}
---
+++ brilliantravensburger/test/brilliantravensburger_test.dart
@@ -0,0 +1,8 @@
+import 'package:brilliantravensburger/brilliantravensburger.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
